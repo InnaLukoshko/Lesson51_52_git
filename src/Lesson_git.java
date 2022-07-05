e6cf7924de98d06efeacc4cbf8dfb993d1eca0ec
ST <inna.rapun@gmail.com>
2022-07-05 20:03:54 +0300

    Добавлена функция умножения

--- src/Lesson_git.java
+++ src/Lesson_git.java
@@ -1,9 +1,14 @@
 public class Lesson_git {
     public static void main(String[] args) {
         sum(2,4);
+        mult(1,3);
    }
    private static int sum(int x, int y) {
         return x + y;
    }
+
+    private static int mult(int x, int y) {
+        return x * y;
+    }
 }
 
