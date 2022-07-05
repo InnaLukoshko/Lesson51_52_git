d08d7ee97956907908625adaaf6cc616744c4f54
ST <inna.rapun@gmail.com>
2022-07-05 20:18:24 +0300

    Добавлен комментарий

--- src/Lesson_git.java
+++ src/Lesson_git.java
@@ -1,7 +1,7 @@
 public class Lesson_git {
     public static void main(String[] args) {
         sum(2,4);
-//        mult(1,3);
+        mult(1,3); // вызов функции умножения
    }
    private static int sum(int x, int y) {
         return x + y;
